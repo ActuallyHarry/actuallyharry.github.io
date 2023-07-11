8b58026f34a98f02ababa9cf80c35ec8502aa160
ActuallyAdequate <actuallyadequate@gmail.com>
2023-07-11 11:14:17 +1000

    navigation changes fix reolve to Home.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './css/App.css'
 import {Routes, Route} from "react-router-dom";
-import Home from './routes/Home';
 import Portfolio from './routes/Portfolio';
 import NavBar from './NavBar';
 import Project from './routes/Project';
