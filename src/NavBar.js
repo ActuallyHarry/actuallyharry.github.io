0f0227e9922de7ca97bbc0ef341a6c3e6ba74320
ActuallyAdequate <actuallyadequate@gmail.com>
2023-07-17 19:42:39 +1000

    NavbarLinks changed

--- src/NavBar.js
+++ src/NavBar.js
@@ -10,8 +10,8 @@ const NavBar = () => {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/portfolio">Portfolio</a></li>
+                    <li className="nav-item"><a className="nav-link" href="https://projects.actuallyadequate.com">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="https://projects.actuallyadequate.com/portfolio">Portfolio</a></li>
                     
                 </ul>
             </div>
